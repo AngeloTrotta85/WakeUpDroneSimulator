cba3dcecefef3de8dab78c529eb39599d27a2102
AngeloTrotta85 <angelo@magilia.com>
2018-12-12 18:21:22 +0100

    ok

--- src/WakeUpDroneSimulator.cpp
+++ src/WakeUpDroneSimulator.cpp
@@ -6,10 +6,55 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <iostream>
-using namespace std;
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>     // std::cout
+#include <fstream>      // std::ifstream
+#include <algorithm>    // std::find
+#include <vector>       // std::vector
+#include <list>       // std::list
+#include <stack>
+#include <map>       // std::list
+#include <cstdlib>
+#include <ctime>
+#include <random>
+#include <chrono>
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+#include <boost/range/irange.hpp>
+#include <boost/math/special_functions/factorials.hpp>
+#include "MyCoord.h"
+
+using namespace std;
+using namespace boost;
+
+class InputParser{
+public:
+	InputParser (int &argc, char **argv){
+		for (int i=1; i < argc; ++i)
+			this->tokens.push_back(std::string(argv[i]));
+	}
+	const std::string& getCmdOption(const std::string &option) const{
+		std::vector<std::string>::const_iterator itr;
+		itr =  std::find(this->tokens.begin(), this->tokens.end(), option);
+		if (itr != this->tokens.end() && ++itr != this->tokens.end()){
+			return *itr;
+		}
+		static const std::string empty_string("");
+		return empty_string;
+	}
+	bool cmdOptionExists(const std::string &option) const{
+		return std::find(this->tokens.begin(), this->tokens.end(), option)
+		!= this->tokens.end();
+	}
+private:
+	std::vector <std::string> tokens;
+};
+
+int main(int argc, char **argv) {
+	cout << "Wake-up Drone BEGIN!!!" << endl;
+
+	InputParser input(argc, argv);
+
+	cout << "Wake-up Drone FINISH!!!" << endl;
+	return EXIT_SUCCESS;
 }
